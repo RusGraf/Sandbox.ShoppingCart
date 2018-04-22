24564c630ef69bb8ca373831ddcd0fc617797be0
Wanl23 <ivan.leschinsky@yandex.ru>
2018-04-22 22:49:01 +0500

    added showing totalPrice

--- Sandbox.ShoppingCart/Models/Cart.cs
+++ Sandbox.ShoppingCart/Models/Cart.cs
@@ -4,19 +4,37 @@ namespace Sandbox.ShoppingCart.Models
 {
     public class Cart
     {
-        public IEnumerable<CartProduct> Products { get; set; }
+        public IEnumerable<CartProduct> Products { get; set; }        
 
         public int ProductCount { get
             {
                 var count = 0;
+                var totalPrice = 0.0d;
                 if (Products != null)
                 {
                     foreach (var product in Products)
                     {                    
                         count += product.QuantityToOrder;
+                        totalPrice += product.Price;
                     }
                 }
                 return count;
+            }
+        }
+
+        public double TotalPrice
+        {
+            get
+            {
+                var totalPrice = 0.0d;
+                if (Products != null)
+                {
+                    foreach (var product in Products)
+                    {
+                        totalPrice += product.Price;
+                    }
+                }
+                return totalPrice;
             }
         }
     }
